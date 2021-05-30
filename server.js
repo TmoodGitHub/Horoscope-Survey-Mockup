ab92e27626183bb5124b5967742ad518fe1ef2bd
TmoodGitHub <tamer.m.mahmoud@gmail.com>
2021-05-29 22:04:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
 
 //commit changes
 app.use(express.static(__dirname));
